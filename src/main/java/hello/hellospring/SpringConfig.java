43782ad4d9c96133f20cedd5d6e4f23583845f9f
ddohing <dnflwlq004@naver.com>
2022-03-04 00:49:25 +0900

    H2 DB를 이용한 스프링 JPA 접근기술 사용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -14,28 +14,35 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-//    private DataSource dataSource;
-    private EntityManager em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+/*    private EntityManager em;
 
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
 
-  /*  @Autowired
+    private DataSource dataSource;
+
+    @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
      }*/
 
         @Bean
         public MemberService memberService () {
-            return new MemberService(memberRepository());
+            return new MemberService(memberRepository);
         }
 
-        @Bean
-        public MemberRepository memberRepository () {
+//        @Bean
+//        public MemberRepository memberRepository () {
 //            return new MemoryMemberRepository();
 //            return new JdbcMemberRepository(dataSource);
 //            return new JdbcTemplateMemberRepository(dataSource);
-            return new JpaMemberRepository(em);
-        }
+//            return new JpaMemberRepository(em);
+
+//        }
     }
