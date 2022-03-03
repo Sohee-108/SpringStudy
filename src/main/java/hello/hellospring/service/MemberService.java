87f6fc1569fab361f21ba172d5a70036fdba05f8
ddohing <dnflwlq004@naver.com>
2022-03-04 01:08:54 +0900

    AOP 사용하기

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -26,9 +26,22 @@ public class MemberService {
      * 회원가입 - 같은 이름이 있는 중복 회원 가입불가
      */
     public Long join(Member member) {
+
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
+
+/*        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("Join = " + timeMs + "ms");
+        }*/
     }
 
     private void validateDuplicateMember(Member member) {
